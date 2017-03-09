9e72185f0a03b5c2fcea9761bbbfbfa45f63d15c
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-03-09 23:02:31 +0700

    add default vendor bundle

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -97,28 +97,34 @@ function run(port) {
 
 // We attempt to use the default port but if it is busy, we offer the user to
 // run on a different port. `detect()` Promise resolves to the next free port.
-detect(DEFAULT_PORT).then(port => {
-  if (port === DEFAULT_PORT) {
-    run(port);
-    return;
-  }
+detect(DEFAULT_PORT).then(
+  port => {
+    if (port === DEFAULT_PORT) {
+      run(port);
+      return;
+    }
 
-  if (isInteractive) {
-    clearConsole();
-    const existingProcess = getProcessForPort(DEFAULT_PORT);
-    const question = chalk.yellow(
-      `Something is already running on port ${DEFAULT_PORT}.` +
-        `${existingProcess ? ` Probably:\n  ${existingProcess}` : ''}`
-    ) + '\n\nWould you like to run the app on another port instead?';
+    if (isInteractive) {
+      clearConsole();
+      const existingProcess = getProcessForPort(DEFAULT_PORT);
+      const question = chalk.yellow(
+        `Something is already running on port ${DEFAULT_PORT}.` +
+          `${existingProcess ? ` Probably:\n  ${existingProcess}` : ''}`
+      ) + '\n\nWould you like to run the app on another port instead?';
 
-    prompt(question, true).then(shouldChangePort => {
-      if (shouldChangePort) {
-        run(port);
-      }
-    });
-  } else {
-    console.log(
-      chalk.red(`Something is already running on port ${DEFAULT_PORT}.`)
-    );
-  }
-});
+      prompt(question, true).then(
+        shouldChangePort => {
+          if (shouldChangePort) {
+            run(port);
+          }
+        },
+        console.log
+      );
+    } else {
+      console.log(
+        chalk.red(`Something is already running on port ${DEFAULT_PORT}.`)
+      );
+    }
+  },
+  console.log
+);
